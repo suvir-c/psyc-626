8bafde226d8eb941709da51d3191211a713359a7
Sandeep Zechariah George <sandeepzgk@gmail.com>
2019-11-06 06:04:38 -0800

    Fix for working on our dataset 1

--- bursty_btm_with_extractor/script/runExample.sh
+++ bursty_btm_with_extractor/script/runExample.sh
@@ -11,8 +11,10 @@ n_day=3
 n_iter=1
 ix_b='n'	# 'n' is not fix, 'y' is fix
 
+rm -rf ../output/
 mkdir -p ../output/eta ../output/doc_wids ../output/model
 input_dir=../sample-data/
+input_dir=extracted/
 output_dir=../output/
 dwid_dir=${output_dir}doc_wids/
 bdf_pt=${output_dir}biterm_dayfreq.txt
@@ -30,11 +32,20 @@ python bitermDayFreq.py $dwid_dir $bdf_pt
 echo "======= Compute eta for Biterms == ======="
 python eta.py $n_day $bdf_pt $eta_dir
 
+echo "###CLEAN###"
+cwd=$(pwd)
+###cd $dwid_dir; find . -name "*.txt" -exec sed -i 's/1/2/g' "{}" \;
+cd $dwid_dir; find . -type f -name "*.txt" -print0 | xargs -0 sed -i '/^[[:space:]]*$/d'
+cd $cwd
+###sed -i '/^[[:space:]]*$/d' 1.txt
+echo "###END-CLEAN###"
+
 ## learning parameters p(z) and p(z|w)
 echo "=============== Topic Learning ============="
 W=`wc -l < $voca_pt`
+rm -f ../src/*.o ## remove old build and rebuild code
 make -C ../src
-for day in `seq 1 2`; do
+for day in `seq 1 $[$n_day-1]`; do
 	echo "---------- day $day --------------"
 	dwid_pt=$dwid_dir$day.txt
 	eta_pt=$eta_dir$day.txt
@@ -45,7 +56,7 @@ done
 
 # ## output top words of each topic
 echo "================ Topic Display ============="
-for day in `seq 1 2`; do
+for day in `seq 1 $[$n_day-1]`; do
 	echo "---------- day $day --------------"
 	res_dir=${model_dir}k$K.day$day.type-$type
 	python topicDisplay.py $voca_pt $res_dir 
