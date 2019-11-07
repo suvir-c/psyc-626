73a357072abab9d61da033a82bd8122dd8a73c8d
Sandeep Zechariah George <sandeepzgk@gmail.com>
2019-11-06 17:05:18 -0800

    Update dataExtrator.py

--- bursty_btm_with_extractor/script/dataExtrator.py
+++ bursty_btm_with_extractor/script/dataExtrator.py
@@ -4,14 +4,14 @@ def do_something_with(line):
 	for item in items:
 		if(itemCount >=1):
 			if(item != ""):
-				with open('extracted/'+str(datefile)+'.txt', 'a+') as the_file:
+				with open('../2m_data/'+str(datefile)+'.txt', 'a+') as the_file:
 					the_file.write(item+'\n')
 		itemCount = itemCount + 1;
 
 
 
 datefile = 0;
-with open("source.csv") as infile:
+with open("../2m_data/re-reservoir_sampling_hydrated_2M_group_by_date_tweet_dic.csv") as infile:
 	for line in infile:
 		do_something_with(line)
 		datefile = datefile + 1 
